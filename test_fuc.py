ba5e3cb4a3b0c4bd6bbcc6423144eb5b41ebc0a2
Seung-been Lee <sbstevenlee@gmail.com>
2021-04-01 20:52:50 +0900

    Update test_fuc.py

--- test_fuc.py
+++ test_fuc.py
@@ -1,14 +1,24 @@
 import unittest
+import pathlib
 
+from VCFResult import VCFResult
 from BEDResult import BEDResult
+
+FUC_DIR = pathlib.Path(__file__).parent.absolute()
+
+class TestVCFResult(unittest.TestCase):
+
+    def test_shape(self):
+        vcf = VCFResult.read(f'{FUC_DIR}/data/1.vcf')
+        self.assertEqual(vcf.shape, (4, 5))
 
 class TestBEDResult(unittest.TestCase):
 
     def test_intersect(self):
-        bed1 = BEDResult.read('data/1.bed')
-        bed2 = BEDResult.read('data/2.bed')
+        bed1 = BEDResult.read(f'{FUC_DIR}/data/1.bed')
+        bed2 = BEDResult.read(f'{FUC_DIR}/data/2.bed')
         bed3 = bed1.intersect(bed2)
-        self.assertEqual(bed3.data, BEDResult.read('data/3.bed').data)
+        self.assertEqual(bed3.data, BEDResult.read(f'{FUC_DIR}/data/3.bed').data)
 
 if __name__ == '__main__':
     unittest.main()
