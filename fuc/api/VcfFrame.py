7f3d3876b5baa0767ac9fe62b1eaa343fa69109f
Seung-been Lee <sbstevenlee@gmail.com>
2021-04-14 20:57:26 +0900

    Update VcfFrame.py

--- fuc/api/VcfFrame.py
+++ fuc/api/VcfFrame.py
@@ -1,46 +1,60 @@
-from dataclasses import dataclass
-
-class VcfFrame:
-    def __init__(self):
-        self.meta = []
-        self.head = []
-        self.data = []
-
-    @classmethod
-    def read(cls, vcf_path):
-        """Create a VCFResult from a file."""
-        vcf_result = cls()
-        if vcf_path.endswith('.gz'):
-            f = gzip.open(vcf_path, 'rb')
-        else:
-            f = open(vcf_path)
-        for line in f:
-            if line.startswith('##'):
-                vcf_result.meta.append(line)
-            elif line.startswith('#CHROM'):
-                fields = line.strip().split('\t')
-                vcf_result.head = fields
-            else:
-                fields = line.strip().split('\t')
-                record = VcfRecord.from_list(fields)
-                vcf_result.data.append(record)
-        f.close()
-        return vcf_result
+from typing import List
+from dataclasses import dataclass, field
 
 @dataclass
 class VcfRecord:
-    chrom : str
-    pos : int
-    id : str
-    ref : str
-    alt : str
-    qual : str
-    filter : str
-    info : str
-    format : str
-    fields : list
+    """Class for storing the information of a single VCF record (row)."""
+    chrom : str = ''
+    pos : int = 0
+    id : str = ''
+    ref : str = ''
+    alt : List[str] = field(default_factory=list)
+    qual : str = ''
+    filter : str = ''
+    info : str = ''
+    format : str = ''
+    gt : List[str] = field(default_factory=list)
 
     @classmethod
     def from_list(cls, l):
-        vf = cls(*l[:9])
+        l[1] = int(l[1])
+        l[4] = l[4].split(',')
+        vf = cls(*l[:9], l[9:])
         return vf
+
+@dataclass
+class VcfFrame:
+    meta : List[str] = field(default_factory=list)
+    head : List[str] = field(default_factory=list)
+    data : List[VcfRecord] = field(default_factory=list)
+
+    @classmethod
+    def from_file(cls, file_path):
+        """Create a VcfFrame from a file."""
+        meta = []
+        head = []
+        data = []
+        if file_path.endswith('.gz'):
+            f = gzip.open(file_path, 'rb')
+        else:
+            f = open(file_path)
+        for line in f:
+            if line.startswith('##'):
+                meta.append(line)
+            elif line.startswith('#CHROM'):
+                head = line.strip().split('\t')
+            else:
+                fields = line.strip().split('\t')
+                record = VcfRecord.from_list(fields)
+                data.append(record)
+        vf = cls(meta, head, data)
+        f.close()
+        return vf
+
+    def to_file(self, file_path):
+        """Write the VcfFrame to a file."""
+        with open(file_path, 'w') as f:
+            f.write(''.join(self.meta))
+            f.write('\t'.join(self.head) + '\n')
+            for record, fields in self.get_data().items():
+                f.write('\t'.join(fields) + '\n')
