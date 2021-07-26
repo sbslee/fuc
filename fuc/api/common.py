c25bc46a3d2ff45b3711d403ddcbcb2b4e64b1de
Seung-been Lee <sbstevenlee@gmail.com>
2021-07-26 20:34:14 +0900

    Add new method common.plot_cytobands

--- fuc/api/common.py
+++ fuc/api/common.py
@@ -14,6 +14,9 @@ import pysam
 import warnings
 import inspect
 from argparse import RawTextHelpFormatter, SUPPRESS
+from matplotlib import pyplot as plt
+from matplotlib.collections import BrokenBarHCollection
+import pandas as pd
 
 FUC_PATH = pathlib.Path(__file__).parent.parent.parent.absolute()
 
@@ -72,7 +75,8 @@ def sumstat(fp, fn, tp, tn):
     return d
 
 def load_dataset(name, force=False):
-    """Load an example dataset from the online repository (requires internet).
+    """
+    Load an example dataset from the online repository (requires internet).
 
     Parameters
     ----------
@@ -103,6 +107,10 @@ def load_dataset(name, force=False):
             'plot_comparison.vcf',
             'normal-tumor.vcf',
             'normal-tumor-annot.tsv',
+        ],
+        'cytoband': [
+            'cytoBandIdeo.txt.gz',
+            'ucsc_genes.bed.gz',
         ],
     }
     base_url = ('https://raw.githubusercontent.com/sbslee/fuc-data/main')
@@ -194,3 +202,144 @@ def extract_sequence(fasta, region):
         warnings.warn(str(e))
         sequence = ''
     return sequence
+
+def plot_cytobands(ideogram, bed, ax=None, figsize=None):
+    """
+    Create chromosome ideograms along with BED data.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes, optional
+        Pre-existing axes for the plot. Otherwise, crete a new one.
+    figsize : tuple, optional
+        Width, height in inches. Format: (float, float).
+
+    Examples
+    --------
+
+    .. plot::
+        :context: close-figs
+
+        >>> import matplotlib.pyplot as plt
+        >>> from fuc import common
+        >>> common.load_dataset('cytoband')
+        >>> cytoband_file = '~/fuc-data/cytoband/cytoBandIdeo.txt.gz'
+        >>> bed_file = '~/fuc-data/cytoband/ucsc_genes.bed.gz'
+        >>> common.plot_cytobands(cytoband_file, bed_file, figsize=(10, 8))
+    """
+    def chromosome_collections(df, y_positions, height, **kwargs):
+        """
+        Yields BrokenBarHCollection of features that can be added to an Axes
+        object.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            Must at least have columns ['chrom', 'start', 'end', 'color']. If no
+            column 'width', it will be calculated from start/end.
+        y_positions : dict
+            Keys are chromosomes, values are y-value at which to anchor the
+            BrokenBarHCollection
+        height : float
+            Height of each BrokenBarHCollection
+        Additional kwargs are passed to BrokenBarHCollection
+        """
+        del_width = False
+        if 'width' not in df.columns:
+            del_width = True
+            df['width'] = df['end'] - df['start']
+        for chrom, group in df.groupby('chrom'):
+            yrange = (y_positions[chrom], height)
+            xranges = group[['start', 'width']].values
+            yield BrokenBarHCollection(
+                xranges, yrange, edgecolors=("black",), facecolors=group['colors'], **kwargs)
+        if del_width:
+            del df['width']
+
+    # Height of each ideogram
+    chrom_height = 1
+
+    # Spacing between consecutive ideograms
+    chrom_spacing = 1
+
+    # Height of the gene track. Should be smaller than `chrom_spacing` in order to
+    # fit correctly
+    gene_height = 0.4
+
+    # Padding between the top of a gene track and its corresponding ideogram
+    gene_padding = 0.1
+
+    # Decide which chromosomes to use
+    chromosome_list = [f'chr{i}' for i in list(range(1, 23)) + ['M', 'X', 'Y']]
+
+    # Keep track of the y positions for ideograms and genes for each chromosome,
+    # and the center of each ideogram (which is where we'll put the ytick labels)
+    ybase = 0
+    chrom_ybase = {}
+    gene_ybase = {}
+    chrom_centers = {}
+
+    # Iterate in reverse so that items in the beginning of `chromosome_list` will
+    # appear at the top of the plot
+    for chrom in chromosome_list[::-1]:
+        chrom_ybase[chrom] = ybase
+        chrom_centers[chrom] = ybase + chrom_height / 2.
+        gene_ybase[chrom] = ybase - gene_height - gene_padding
+        ybase += chrom_height + chrom_spacing
+
+    # Read in ideogram.txt, downloaded from UCSC Table Browser
+    ideo = pd.read_table(
+        ideogram,
+        names=['chrom', 'start', 'end', 'name', 'gieStain']
+    )
+
+    # Filter out chromosomes not in our list
+    ideo = ideo[ideo.chrom.apply(lambda x: x in chromosome_list)]
+
+    # Add a new column for width
+    ideo['width'] = ideo.end - ideo.start
+
+    # Colors for different chromosome stains
+    color_lookup = {
+        'gneg': (1., 1., 1.),
+        'gpos25': (.6, .6, .6),
+        'gpos50': (.4, .4, .4),
+        'gpos75': (.2, .2, .2),
+        'gpos100': (0., 0., 0.),
+        'acen': (.8, .4, .4),
+        'gvar': (.8, .8, .8),
+        'stalk': (.9, .9, .9),
+    }
+
+    # Add a new column for colors
+    ideo['colors'] = ideo['gieStain'].apply(lambda x: color_lookup[x])
+
+    # Same thing for genes
+    genes = pd.read_table(
+        bed,
+        names=['chrom', 'start', 'end', 'name'],
+        usecols=range(4))
+    genes = genes[genes.chrom.apply(lambda x: x in chromosome_list)]
+    genes['width'] = genes.end - genes.start
+    genes['colors'] = '#2243a8'
+
+    # Determine which matplotlib axes to plot on.
+    if ax is None:
+        fig, ax = plt.subplots(figsize=figsize)
+
+    # Now all we have to do is call our function for the ideogram data...
+    for collection in chromosome_collections(ideo, chrom_ybase, chrom_height):
+        ax.add_collection(collection)
+
+    # ...and the gene data
+    for collection in chromosome_collections(
+        genes, gene_ybase, gene_height, alpha=0.5, linewidths=0
+    ):
+        ax.add_collection(collection)
+
+    # Axes tweaking
+    ax.set_yticks([chrom_centers[i] for i in chromosome_list])
+    ax.set_yticklabels(chromosome_list)
+    ax.axis('tight')
+
+    return ax
